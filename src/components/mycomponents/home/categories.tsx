876a8bd6b0ac203a8a6144cddabc755f153e491e
Papp Dávid <impappdavid@gmail.com>
2025-07-20 02:52:10 +0200

    Changing the categories link animation and adding a title

--- src/components/mycomponents/home/categories.tsx
+++ src/components/mycomponents/home/categories.tsx
@@ -8,15 +8,24 @@ function Categories() {
 
     return (
         <>
-            <div className="flex flex-row gap-2 p-4 ">
-                <Link to={`/just-chatting`} className="flex flex-col gap-1">
-                    <img src="/categories/just-chatting.png" alt="just-chatting" className="max-w-36 rounded-lg"/>
-                    <div className="text-sm text-zinc-200">Just Chatting</div>
-                </Link>
-                <Link to={`/players`} className="flex flex-col gap-1">
-                    <img src="/categories/gamers.png" alt="gamers" className="max-w-36 rounded-lg"/>
-                    <div className="text-sm text-zinc-200">Gamers</div>
-                </Link>
+            <div className="flex flex-col gap-4">
+                <div className="flex w-full justify-between items-center">
+                    <div className="text-xl font-medium">{t("Categories")}</div>
+                </div>
+                <div className="flex flex-row gap-2 ">
+                    <Link to={`/just-chatting`} className="flex flex-col gap-1 group">
+                        <div className="overflow-hidden rounded-lg">
+                            <img src="/categories/just-chatting.png" alt="just-chatting" className="max-w-36 rounded-lg transition-transform duration-300 group-hover:scale-105 transform-gpu" />
+                        </div>
+                        <div className="text-sm text-zinc-200 group-hover:text-blue-400 transition-colors duration-300">Just Chatting</div>
+                    </Link>
+                    <Link to={`/players`} className="flex flex-col gap-1 group">
+                        <div className="overflow-hidden rounded-lg">
+                            <img src="/categories/gamers.png" alt="gamers" className="max-w-36 rounded-lg transition-transform duration-300 group-hover:scale-105 transform-gpu" />
+                        </div>
+                        <div className="text-sm text-zinc-200 group-hover:text-green-500 transition-colors duration-300">Gamers</div>
+                    </Link>
+                </div>
             </div>
         </>
     )
