3bdb4d80f3c0c5ad0bc26bc14a53753f66eaff41
Papp Dávid <impappdavid@gmail.com>
2025-07-27 01:19:08 +0200

    Refactor drawerMenu.tsx

--- src/components/mycomponents/navbar/drawerMenu.tsx
+++ src/components/mycomponents/navbar/drawerMenu.tsx
@@ -12,13 +12,12 @@ import { BadgeCheck, CircleQuestionMark, House, Info, Menu, MessagesSquare, Musi
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-
 function DrawerMenu() {
-
-    const baseClass = "flex gap-2 items-center  p-2 rounded-lg transition-all duration-200"
-
+    // Used for highlight toggle of creator button (demo toggler)
     const [isCreator, setIsCreator] = useState(false);
     const { t } = useTranslation()
+    const baseClass = "flex gap-2 items-center  p-2 rounded-lg transition-all duration-200"
+
     return (
         <>
             <Drawer direction="left">
@@ -75,6 +74,7 @@ function DrawerMenu() {
                                     ? `${baseClass} bg-[#19FF00] text-black  fill-black`
                                     : `${baseClass} text-[#19FF00]  hover:text-[#1aff00c0] fill-[#19FF00] hover:fill-[#1aff00c0]`
                             }>
+                                {/* Tiktok icon */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" className="fill-[#2856F4]">
                                     <path d="M16.6 5.82s.51.5 0 0A4.28 4.28 0 0 1 15.54 3h-3.09v12.4a2.59 2.59 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48" />
                                 </svg>
@@ -96,16 +96,16 @@ function DrawerMenu() {
                                 <div className="h-[2px] w-full bg-zinc-900"></div>
                             </div>
                             <div className="flex flex-col gap-1 p-2 ">
+                                {/* Demo: Become/Listing toggle */}
                                 {isCreator ? (
                                     <NavLink to={"/create-listing"} className={`${baseClass} bg-[#1aff00c0] cursor-pointer border border-dashed border-green-500/50 text-black hover:bg-[#19FF00]`}>
                                         <Plus className="w-5 h-5" />
-                                        <div className="text-md font-medium hidden xl:flex">Crete listing</div>
+                                        {/* Typo fix: "Create listing" */}
+                                        <div className="text-md font-medium hidden xl:flex">Create listing</div>
                                     </NavLink>
                                 ) : (
                                     <NavLink to="" onClick={() => setIsCreator(!isCreator)} className={`${baseClass} bg-[#1aff00c0] border border-dashed border-green-500/50 text-black hover:bg-[#19FF00]`}><BadgeCheck className="w-5 h-5" /> <div className="text-md font-medium hidden xl:flex">{t("Become")}</div></NavLink>
-
                                 )}
-
                             </div>
                             <div className="px-2 py-1.5">
                                 <div className="h-[2px] w-full bg-zinc-900"></div>
@@ -115,15 +115,12 @@ function DrawerMenu() {
                                     ? `${baseClass} bg-[#19FF00] text-black  fill-black`
                                     : `${baseClass} text-[#19FF00]  hover:text-[#1aff00c0]`
                             }><CircleQuestionMark className="w-5 h-5 text-[#2856F4]" /> <div className="text-md font-medium ">FAQ</div></NavLink>
-
                             <NavLink to="../about-us" className={({ isActive }) =>
                                 isActive
                                     ? `${baseClass} bg-[#19FF00] text-black  fill-black`
                                     : `${baseClass} text-[#19FF00]  hover:text-[#1aff00c0]`
                             }><Info className="w-5 h-5 text-[#2856F4]" /> <div className="text-md font-medium">{t("About")}</div></NavLink>
-
                         </div>
-
                     </div>
                 </DrawerContent>
             </Drawer>
