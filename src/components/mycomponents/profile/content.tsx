09762d4a46f39fc774940c21df60cdc9ad14bd77
Papp Dávid <impappdavid@gmail.com>
2025-07-27 01:32:02 +0200

    Refactor content.tsx

--- src/components/mycomponents/profile/content.tsx
+++ src/components/mycomponents/profile/content.tsx
@@ -191,8 +191,6 @@ function Content() {
                         ) : (
                             <>
                                 <Services userId={userId} />
-
-
                             </>
                         )}
                     </div>
