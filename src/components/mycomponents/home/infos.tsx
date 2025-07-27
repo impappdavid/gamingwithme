e5ba43232bf560e9c6218689fb0796f9dbd595b7
Papp Dávid <impappdavid@gmail.com>
2025-07-27 02:26:48 +0200

    Implementing the translation

--- src/components/mycomponents/home/infos.tsx
+++ src/components/mycomponents/home/infos.tsx
@@ -1,20 +1,22 @@
+import { useTranslation } from "react-i18next"
 import { Link } from "react-router-dom"
 
 function Infos() {
+    const { t } = useTranslation()
     return (
         <>
             <div className="flex flex-row gap-2 w-full">
                 <Link to={'aboutus'} className="w-fit flex justify-center items-center text-sm text-zinc-400 hover:bg-zinc-950 p-2 rounded-lg hover:text-white transition-all duration-300">
-                    ABOUT US
+                    {t("ABOUT US")}
                 </Link>
                 <Link to={'howitworks'} className="w-fit flex justify-center items-center text-sm text-zinc-400 hover:bg-zinc-950 p-2 rounded-lg hover:text-white transition-all duration-300">
-                    HOW IT WORKS?
+                    {t("HOW IT WORKS?")}
                 </Link>
                 <Link to={'download'} className="w-fit flex justify-center items-center text-sm text-zinc-400 hover:bg-zinc-950 p-2 rounded-lg hover:text-white transition-all duration-300">
-                    Download
+                    {t("DOWNLOAD")}
                 </Link>
                 <Link to={'news'} className="w-fit flex justify-center items-center text-sm text-zinc-400 hover:bg-zinc-950 p-2 rounded-lg hover:text-white transition-all duration-300">
-                    NEWS
+                    {t("NEWS")}
                 </Link>
             </div>
         </>
